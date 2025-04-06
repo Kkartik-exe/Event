6f11ef6113356629095bc7b8787c3bebb92a1005
Kkartik-exe <nayanavisave@gmail.com>
2025-04-06 16:59:21 -0700

    Add files via upload

---
+++ src/Module/General.js
@@ -0,0 +1,437 @@
+import team from "./Assets/team.gif";
+import digilogo from "./Assets/sponsorsLogos/digilogo.png";
+import devfolio from "./Assets/sponsorsLogos/devfolio.png";
+import filecoin from "./Assets/sponsorsLogos/filecoin.png";
+import polygon from "./Assets/sponsorsLogos/polygon.png";
+import shutterbugs from "./Assets/sponsorsLogos/shutterbugs.png";
+//import alan from "./Assets/sponsorsLogos/alan.png";
+import koii from "./Assets/sponsorsLogos/koii.png";
+// import celo from "./Assets/sponsorsLogos/celo.png";
+import echo3d from "./Assets/sponsorsLogos/echo3d.png";
+//port streamyard from "./Assets/sponsorsLogos/streamyard1.JPG";
+import wolfram from "./Assets/sponsorsLogos/wolfram.jpg";
+import gfg from "./Assets/sponsorsLogos/gfg.png";
+import certopus from "./Assets/sponsorsLogos/certopus.png";
+import prayosys from "./Assets/sponsorsLogos/prayosys.png";
+//import shivalika from "./Assets/sponsorsLogos/shivalika.JPG";
+//import shivam from "./Assets/sponsorsLogos/shivam.JPG";
+import jay_maharaj from "./Assets/sponsorsLogos/jay_maharaj.jpeg";
+import jay_bhavani from "./Assets/sponsorsLogos/jay_bhavani.jpeg";
+import maruti from "./Assets/sponsorsLogos/maruti.jpeg";
+import replit from "./Assets/sponsorsLogos/replit.png";
+import taskade from "./Assets/sponsorsLogos/taskade.png";
+import interviewbuddy from './Assets/sponsorsLogos/ib.png'
+import digitalocean from './Assets/sponsorsLogos/do.png'
+//import cuvette from './Assets/sponsorsLogos/cuvette.png'
+import postman from './Assets/sponsorsLogos/pm.svg'
+
+import zenisha from "./Assets/teami/Zenisha_Savaliya.jpeg";
+import dhruvi from "./Assets/teami/Dhruvi_Sherathiya.jpg";
+
+import me from "./Assets/teami/Vashishth_Patel.jpg";
+import dharmesh from "./Assets/teami/Dharmesh_Poriya.jpg";
+import Ryah from "./Assets/teami/Ryah.jpg";
+import nirali from "./Assets/teami/Nirali.jpeg";
+import smit from "./Assets/teami/smit.jpeg";
+import manav from "./Assets/teami/manav.jpg";
+// import ruchit from "./Assets/teami/Ruchit_Parekh.jpg";
+import parshwa from "./Assets/teami/Parshwa_Mehta.jpg";
+import ayush from "./Assets/teami/Ayush_Rudani.jpg";
+import aneri from "./Assets/teami/Aneri_Sonani.jpg";
+
+
+
+
+
+
+
+
+const TOP_SECTION = {
+  TITLE: "NURO-NOVA",
+  Typed_effect: ["08 May - 10 May", "Venue BIGCE", "Win awesome prizes", "Events And Sessions"],
+  SHORT_DESCRIPTION:
+    "Join us on 08 and 10th of May 2025 , innovation, & fun with over 1000+ students.                    ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎                                                     Click 👇 below Icon To Join Official Whatsapp Group, Stay Updated",
+  IMG_SRC: team,
+  DISCORD_LINK: "https://chat.whatsapp.com/JilfTvOnqsCI8Wnu0pVAal",
+  JUDGES_FORM_LINK:
+    "#Jugdes Form Link",
+  HACKERS_REGISTRATION_FORM_LINK:
+    "#Hackers Registration devfolio link"
+};
+
+const SOCIALS = {
+  instagram: "https://www.instagram.com/the.kkartik?igsh=NTc4MTIwNjQ2YQ==",
+  discord: "",
+  linkedin: "http://kartikportfolio.rf.gd/?i=1",
+  twitter: "",
+  email: "kartikkothalkar4@gmail.com",
+  mail: "kartikkothalkar4@gmail.com"
+};
+
+const MIDDLE_SECTION = {
+  TITLE: "What is NURO-NOVA?",
+  LONG_DESCRIPTION:
+    "Nuro-Nova is a competition organized by Bharat Ratna Engineering College, Solapur, which brings together students to showcase their skills in both technical and non-technical domains. This event aims to foster creativity, innovation, and teamwork, providing a platform for students to demonstrate their knowledge and talent. The competition features a variety of challenges that cater to different interests, offering opportunities for participants to engage in intellectual and practical problem-solving, while also enhancing their learning experience. Whether you're a tech enthusiast or someone with a flair for non-technical activities, Nuro-Nova serves as an exciting avenue to grow, compete, and excel.",
+  LOGO_EFFECT: true,
+  LOGO: ""
+};
+
+const FOOTER = {
+  VOLUNTEERING_FORM: {
+    required: true,
+    src: "#Volunteer form link"
+  },
+  JOIN_TEAM: {
+    required: true,
+    src: "#Join team link"
+  },
+  Privacy_policy: {
+    required: true,
+    src: ""
+  },
+  Terms_of_use: {
+    required: true,
+    src: ""
+  }
+};
+
+const calenderStartingDate = {
+  month: 8,
+  year: 2025
+}
+
+
+const schedule = [
+  {
+    "day": "16-7-2022",
+    "events": [
+      {
+        "title": "Opening Ceremoney",
+        "timings": "8 AM - 9 AM",
+        "link": "https://google.com"
+      }
+    ]
+  },
+  {
+    "day": "16-7-2022",
+    "events": [
+      {
+        "title": "Event",
+        "timings": "11 AM - 12 PM",
+        "link": "https://google.com"
+      }
+    ]
+  },
+  {
+    "day": "16-7-2022",
+    "events": [
+      {
+        "title": "Event",
+        "timings": "12 AM - 1 Pm",
+        "link": "https://google.com"
+      }
+    ]
+  },
+  {
+    "day": "17-7-2022",
+    "events": [
+      {
+        "title": "Event",
+        "timings": "12 AM - 1 Pm",
+        "link": "https://google.com"
+      }
+    ]
+  },
+  {
+    "day": "17-7-2022",
+    "events": [
+      {
+        "title": "Closing Ceremoney",
+        "timings": "6 PM - 7 PM",
+        "link": "https://google.com"
+      }
+    ]
+  },
+]
+
+const Prizeinfo = [
+  [
+    //Array 1
+    {
+      icon: <i className="first fas fa-4x fa-trophy"></i>,
+      type: "Overall First",
+      content:
+        "The first overall prize will be given to a project that outstands all other submissions."
+    },
+    {
+      icon: <i className=" second fas fa-4x fa-medal"></i>,
+      type: "Overall Second",
+      content:
+        "The second overall prize will be given to the second-best project in the hackathon."
+    },
+    {
+      icon: <i className=" third fas fa-4x fa-skiing-nordic"></i>,
+      type: "Overall Third",
+      content:
+        "The third overall prize will be given to the third-best project in the hackathon."
+    }
+  ],
+  [
+    //Array 2
+    {
+      icon: <i className="first fas fa-4x fa-trophy"></i>,
+      type: " Best Women Team",
+      content: "Your project will qualify for this category if your all team members are women."
+    },
+    {
+      icon: <i className="first fas fa-4x fa-trophy"></i>,
+      type: "Best IEEE Team",
+      content: "Your project will qualify for this category if your all team members are from IEEE."
+    },
+    {
+      icon: <i className="fas fa-user-friends seventh fa-3x "></i>,
+      type: "More prizes",
+      content: "More prizes will be revealed later"
+    }
+  ],
+  [
+    //Array 3
+  ]
+];
+
+const TeamInfo = [
+  [
+    // Array 1
+    {
+      Name: "Er.Ravindra Gaikwad",
+      role: "President",
+      img: me
+    },
+    {
+      Name: "Dr. Anil Deshmane",
+      role: "Principal",
+      img: ayush
+    },
+    {
+      Name: "Prof. Anand Gyanbote",
+      role: "HOD Of CSE",
+      img: smit
+    }
+  ],
+  [
+    // Array 2
+    {
+      Name: "Dr.Sohel Rana",
+      role: "Staff Organizer",
+      github: "",
+      linkedin: "",
+      img: zenisha
+    },
+    {
+      Name: "Prof. Sameer Shaikh",
+      role: "Staff Organizer",
+      github: "",
+      linkedin: "",
+      img: parshwa
+    },
+    {
+      Name: "Prashant Nandur",
+      role: "Systems Analyst",
+      github: "",
+      linkedin: "",
+      img: dharmesh
+    },
+  ],
+  [
+    // Array 2
+    {
+      Name: "Kartik Kothalkar",
+      role: "Main Co-ordinator",
+      github: "",
+      linkedin: "",
+      img: aneri
+    },
+    {
+      Name: "Suraj Maru",
+      role: "Main Co-ordinator",
+      github: "#",
+      linkedin: "",
+      img: nirali
+    },
+    {
+      Name: "Minaj Shaikh",
+      role: "Main Co-ordinator",
+      img: dhruvi
+    },
+  ],
+  [
+    // Array 2
+    {
+      Name: "Rohan Jadhav",
+      role: "Main Co-ordinator",
+      img: manav
+    }
+  ]
+  
+];
+
+const JudgesInfo = [
+  [
+    // Array 1
+    {
+      Name: "Rehan",
+      role: "Organizer",
+      github: "",
+      linkedin: "",
+      img: me
+    },
+    {
+      Name: "Moon",
+      role: "Manager",
+      github: "",
+      linkedin: "",
+      img: Ryah
+    },
+    {
+      Name: "Ryah Garcia",
+      role: "Organizer",
+      github: "",
+      linkedin: "",
+      img: Ryah
+    }
+  ],
+  [
+    // Array 2
+    {
+      Name: "Lyanola",
+      role: "Organizer",
+      github: "",
+      linkedin: "",
+      img: Ryah
+    },
+    {
+      Name: "Zoheb",
+      role: "Organizer",
+      github: "",
+      linkedin: "",
+      img: Ryah
+    }
+  ]
+];
+
+const sponsorLogos = [
+  // [{ src: cfc }, { src: sublime }, { src: echoAR }], //Array 1
+  // [{ src: taskade }, { src: Replit }, { src: qoom }], //Array 2
+  // [{ src: interviewC }, { src: pass }, { src: glimpse }], //Array 3
+  // [{ src: egg }, { src: ll }, { src: ACF }] //Array 4
+
+];
+
+const titlesponsorLogos = [
+  [{ src: devfolio, link: "" }, { src: polygon, link: "" }], //Array 1
+  // [{ src: taskade }, { src: Replit }, { src: qoom }], //Array 2
+  // [{ src: interviewC }, { src: pass }, { src: glimpse }], //Array 3
+  // [{ src: egg }, { src: ll }, { src: ACF }] //Array 4
+
+];
+
+const goldsponsorLogos = [
+  // [  ], //Array 1
+  [{ src: filecoin, link: "" }, { src: gfg, link: "" }, { src: digitalocean, link: "" }], //Array 2
+  // [{ src: interviewC }, { src: pass }, { src: glimpse }], //Array 3
+  // [{ src: egg }, { src: ll }, { src: ACF }] //Array 4
+
+];
+
+const silversponsorLogos = [
+  [{ src: echo3d, link: "" }, { src: wolfram, link: "" }, { src: interviewbuddy, link: "" }], //Array 1
+ // [{ src: alan, link: "https://alan.app/" }, ] //{ src: shivam, link: "#" }],
+  // [{ src: taskade }, { src: Replit }, { src: qoom }], //Array 2
+  // [{ src: interviewC }, { src: pass }, { src: glimpse }], //Array 3
+  // [{ src: egg }, { src: ll }, { src: ACF }] //Array 4
+
+];
+
+const bronzeponsorLogos = [
+  [{ src: replit, link: "" }, { src: certopus, link: "" }, { src: taskade, link: "" },], //Array 1
+ // [{ src: koii, link: "https://www.koii.network/" }, { src: postman, link: "https://postman.com/" }, {src: prayosys, link: "https://prayosys.com/"}],
+ // [{src: shivalika, link: "#"}, {src: cuvette, link: "https://www.cuvette.tech/"}],
+  // [{ src: taskade }, { src: Replit }, { src: qoom }], //Array 2
+  // [{ src: interviewC }, { src: pass }, { src: glimpse }], //Array 3
+  // [{ src: egg }, { src: ll }, { src: ACF }] //Array 4
+
+];
+
+const FoodLogos = [
+  [{src: maruti, link: "https://bigce.in/"},]//{src: jay_bhavani, link: "#"},{src: jay_maharaj, link: "#"}],
+];
+
+const designlogo = [
+  [{ src: digilogo, link: "https://bigce.in/" }, { src: shutterbugs, link: "https://thelifenavigator.com/" }]
+];
+
+
+const frequentlyAskedQuestions = [
+  [
+    [
+      {
+        label: "What is a Nuro-Nova?",
+        content:
+          'Nuro-Nova is a competition organized by Bharat Ratna Engineering College, Solapur, which brings together students to showcase their skills in both technical and non-technical domains..'
+      },
+      {
+        label: "Who can attend?",
+        content: "Nuro-Nova is open to all undergraduate, high school students, and middle school students from all schools. Unfortunately, tudents can attend this event offline. For the rest of the other students, the event would be online."
+      },
+      {
+        label: "I am a first time Participate, what should I do?",
+        content: "No worries, we love to welcome first-time Participent and start their journey from a Event. We have some awesome workshops and events which will help you get started with Events in general."
+      },
+      {
+        label: "How team formation works?",
+        content: "You can form a team with 4 members in it at maximum. If you don't have a team, there'll be plenty of opportunities for you to meet Participent and form a team."
+      }
+    ],
+    [
+      {
+        label: "How to register myself in the Nuro-Nova?",
+        content: "All you need is to register yourself on The life Navigator and join Whatsapp Group Of Each Event, we will guide you through everything there."
+      },
+      {
+        label: "I have more questions?",
+        content: "Reach us directly at (Whatsapp:-9172982788) we would happy to help you."
+      }
+      ,
+      {
+        label: "Can I participate as a individual?",
+        content: "Yes, Or Better You Have team Because our goal is collaborative learning, we encourage everyone to participate as a team of at least two people."
+      }
+      ,
+      {
+        label: "Will there be Certificates?",
+        content: "Yes, Participants and winners will receive, certificates and much more!"
+      }
+    ]
+  ],
+
+];
+
+export {
+  TOP_SECTION,
+  MIDDLE_SECTION,
+  SOCIALS,
+  FOOTER,
+  Prizeinfo,
+  sponsorLogos,
+  titlesponsorLogos,
+  goldsponsorLogos,
+  silversponsorLogos,
+  bronzeponsorLogos,
+  TeamInfo,
+  JudgesInfo,
+  frequentlyAskedQuestions,
+  schedule,
+  calenderStartingDate,
+  designlogo,
+  FoodLogos
+};
